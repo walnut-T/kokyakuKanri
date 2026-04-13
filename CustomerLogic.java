0fc04742222398f6ab8e9ab1fe4c558112c70c11
ori-0928 <sky200213ktr@gmail.com>
2026-04-13 16:52:23 +0900

    Add files via upload

---
+++ CustomerLogic.java
@@ -0,0 +1,91 @@
+package customer;
+
+import javax.servlet.http.HttpServletRequest;
+
+import dao.CustomerDao;
+import util.LogUtil;
+import util.StringUtil;
+
+/**
+ * 顧客情報のロジック
+ */
+public class CustomerLogic {
+
+	/**
+	 * DAO経由で顧客情報をDBから取得する
+	 * @param id 取得する顧客情報のID
+	 * @return 顧客情報Bean
+	 */
+	public CustomerBean load(int id) {
+		LogUtil.println(this.getClass().getSimpleName() + "#load");
+
+		CustomerDao customerDao = new CustomerDao();
+		CustomerBean customer = customerDao.load(id);
+
+		return customer;
+	}
+
+	/**
+	 * DAO経由で顧客情報をDBに追加する
+	 * @param customer 顧客情報Bean
+	 * @return エラーメッセージ(処理成功時、null)
+	 */
+	public String add(CustomerBean customer) {
+		LogUtil.println(this.getClass().getSimpleName() + "#add");
+
+		// TODO 未実装
+
+		return null;
+	}
+
+	/**
+	 * DAO経由でDBの顧客情報を更新する
+	 * @param customer 顧客情報Bean
+	 * @return エラーメッセージ(処理成功時、null)
+	 */
+	public String update(CustomerBean customer) {
+		LogUtil.println(this.getClass().getSimpleName() + "#update");
+
+		// TODO 未実装
+
+		return null;
+	}
+
+	/**
+	 * DAO経由でDBの顧客情報を削除する
+	 * @param customer 顧客情報Bean
+	 * @return エラーメッセージ(処理成功時、null)
+	 */
+	public String delete(CustomerBean customer) {
+		LogUtil.println(this.getClass().getSimpleName() + "#delete");
+
+		// TODO 未実装
+
+		return null;
+	}
+
+	/**
+	 * リクエスト内の顧客情報をセッションに顧客情報Beanとして設定する
+	 * @param request 顧客情報を含むリクエスト
+	 */
+	public void setCustomerBeanFromRequestToSession(HttpServletRequest request) {
+		LogUtil.println(this.getClass().getSimpleName() + "#setCustomerBeanFromRequestToSession");
+
+		// TODO 未実装（4/11折原実装中）
+		CustomerBean customer = (CustomerBean) request.getSession().getAttribute("customer");
+		if (customer == null) {
+			customer = new CustomerBean();
+		}
+
+		customer.setName(StringUtil.exchangeESCEncoding(request.getParameter("user_name")));
+		customer.setZip(StringUtil.exchangeESCEncoding(request.getParameter("post_code")));
+		customer.setAddress1(StringUtil.exchangeESCEncoding(request.getParameter("address1")));
+		customer.setAddress2(StringUtil.exchangeESCEncoding(request.getParameter("address2")));
+		customer.setTel(StringUtil.exchangeESCEncoding(request.getParameter("telephone_number")));
+		customer.setFax(StringUtil.exchangeESCEncoding(request.getParameter("fax_number")));
+		customer.setEmail(StringUtil.exchangeESCEncoding(request.getParameter("e-mail")));
+		
+		request.setAttribute("customer", customer);
+		return;
+	}
+}
